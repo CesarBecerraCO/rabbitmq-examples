dd7ad94b557e1544650ca502cf1e3c932c978a23
Cesar Becerra <cesar.becerra.co@gmail.com>
2023-09-06 20:16:08 -0500

    Fanout exchange example

---
+++ fanout_consumer.py
@@ -0,0 +1,31 @@
+import pika, os, sys
+from dotenv import load_dotenv
+load_dotenv()
+
+def main():
+    eName, eType, qName = "eventos", "fanout", "qf"
+    connection = pika.BlockingConnection(pika.URLParameters(os.getenv('MQ_URL')))
+    channel = connection.channel()
+    channel.exchange_declare(exchange=eName, exchange_type=eType)
+    channel.queue_declare(queue=qName)
+    channel.queue_bind(queue=qName, exchange=eName, routing_key='')
+    
+    def msg_consumer(ch, method, properties, body):
+        print(f" [x] Received {body}")
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+    
+    #channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue=qName, on_message_callback=msg_consumer)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
---
+++ fanout_producer.py
@@ -0,0 +1,30 @@
+import pika, os, sys, time
+from dotenv import load_dotenv
+load_dotenv()
+
+def main():
+    eName, eType, qName = "eventos", "fanout", "qf"
+    connection = pika.BlockingConnection(pika.URLParameters(os.getenv('MQ_URL')))
+    channel = connection.channel()
+    channel.exchange_declare(exchange=eName, exchange_type=eType)
+    channel.queue_declare(queue=qName)
+
+    i = 0
+    while(True):
+        msg_body=f"Evento {i}"
+        print(f" [x] Sent '{msg_body}'")
+        channel.basic_publish(exchange=eName, routing_key='', body=msg_body)
+        time.sleep(2)
+        i += 1
+
+    connection.close()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
